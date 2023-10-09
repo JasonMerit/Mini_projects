e04bbaeb233106e00bc5cac797b65a0ee6fa6a48
JasonMerit <jasonaronmerit@gmail.com>
2023-10-09 14:19:19 +0200

    RPS init

---
+++ Apps/ToDoList/ToDoList/ToDoList/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<TaskItem> tasks = new ObservableCollection<TaskItem>();
+        public MainPage()
+        {
+            InitializeComponent();
+            tasks.Add(new TaskItem { Title = "Buy groceries", IsCompleted = false });
+            taskListView.ItemsSource = tasks;
+        }
+
+        private void AddTaskButton_Clicked(object sender, EventArgs e)
+        {
+            string newTaskTitle = newTaskEntry.Text;
+            if (!string.IsNullOrWhiteSpace(newTaskTitle))
+            {
+                tasks.Add(new TaskItem { Title = newTaskTitle, IsCompleted = false });
+                newTaskEntry.Text = ""; // Clear the entry field after adding
+            }
+        }
+
+    }
+
+    public class TaskItem
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+
+}
---
+++ Apps/ToDoList/ToDoList/ToDoList/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using ToDoList;
+
+namespace ToDoList
+{
+	public class MainViewModel
+	{
+		public ObservableCollection<TaskItem> Tasks { get; set; }
+
+		public MainViewModel() 
+		{ 
+			Tasks = new ObservableCollection<TaskItem>();
+		}
+
+		public ICommand AddTaskCommand => new Command(AddTask);
+	}
+}
+
